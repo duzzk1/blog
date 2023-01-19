d158f046e0f60eecc2c3be26007ab5af47b3d3c0
Eduardo Santos <eduluis.cco@gmail.com>
2023-01-18 22:55:12 -0300

    Create the structure of components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import Header from "./components/Header";
+import Article from "./components/Articles";
+import Posts from "./components/Posts";
+
 function App() {
-  return <h1>My blog</h1>;
+  return (
+    <>
+      retornar mais elementos deve se usar esta sintaxe
+      <Header></Header>
+      <Article></Article>
+      <Posts></Posts>
+      <h1>teste</h1>;
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Posts/index.js
@@ -0,0 +1,5 @@
+const Posts = () => {
+  return <h1>Posts.js</h1>;
+};
+
+export default Posts;
