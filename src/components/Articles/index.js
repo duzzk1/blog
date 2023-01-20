06657b4de0175b174fd65b19f03ef42e778937ae
Eduardo Santos <eduluis.cco@gmail.com>
2023-01-19 22:04:11 -0300

    Learning about PROPS

--- src/components/Articles/index.js
+++ src/components/Articles/index.js
@@ -1,13 +1,33 @@
 import Posts from "../Posts";
 
 const Article = () => {
+  // Create a requisition for an API.
+
   return (
     <section className="container">
       <h1 className="tac">Articles</h1>
       <div className="container-posts mt-4">
-        <Posts />
-        <Posts />
-        <Posts />
+        <Posts
+          subtitle="tecnologia"
+          title="O primeiro post do blog"
+          creatorName="Eduardo Luis"
+        >
+          1oioioio oiuoio
+        </Posts>
+        <Posts
+          subtitle="fotografia"
+          title="O segundo post do blog"
+          creatorName="Eduardo Santos"
+        >
+          2oioioio oiuoio
+        </Posts>
+        <Posts
+          subtitle="cinema"
+          title="O terceiro post do blog"
+          creatorName="Duzzk"
+        >
+          3oioioio oiuoio
+        </Posts>
       </div>
     </section>
   );
--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,18 +1,14 @@
-const Posts = () => {
+const Posts = (props) => {
   return (
     <div className="post mx-2">
       <div className="img-posts"></div>
-      <h5>Tech</h5>
-      <h3>O primeiro post do blog</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam.
-      </p>
+      <h5>{props.subtitle}</h5>
+      <h3>{props.title}</h3>
+      <p>{props.children}</p>
       <div className="flex pt-2">
         <div className="img-profile"></div>
         <div className="desc-profile  ml-2">
-          <h6 className="color-blue">Eduardo Santos</h6>
+          <h6 className="color-blue">{props.creatorName}</h6>
           <p>Jan 18, 2023 - 2 min read</p>
         </div>
       </div>
